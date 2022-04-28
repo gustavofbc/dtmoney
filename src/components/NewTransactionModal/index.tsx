224f35c455093334cc962cb6579acd5bb11be136
gustavofbc <gustavo.felipebc@gmail.com>
2022-04-28 16:52:38 -0300

    funcionamento dos botoes

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,7 +3,8 @@ import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
 
-import { Container, TransactionTypeContainer } from "./styles";
+import { Container, RadioBox, TransactionTypeContainer } from "./styles";
+import { useState } from "react";
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -14,6 +15,9 @@ export function NewTransaciontModal({
     isOpen,
     onRequestClose,
 }: NewTransactionModalProps) {
+
+    const [type, setType] = useState('deposit');
+
     return (
         <Modal
             isOpen={isOpen}
@@ -42,18 +46,26 @@ export function NewTransaciontModal({
                 />
 
                 <TransactionTypeContainer>
-                    <button
+                    <RadioBox
                         type="button"
+                        onClick={() => {
+                            setType('deposit')
+                        }}
+                        isActive={type === "deposit"}
                     >
                         <img src={incomeImg} alt="Entrada" />
                         <span>Entrada</span>
-                    </button>
-                    <button
+                    </RadioBox>
+                    <RadioBox
                         type="button"
+                        onClick={() => {
+                            setType('withdraw')
+                        }}
+                        isActive={type === "withdraw"}
                     >
                         <img src={outcomeImg} alt="Saída" />
                         <span>Saída</span>
-                    </button>
+                    </RadioBox>
                 </TransactionTypeContainer>
 
                 <input
